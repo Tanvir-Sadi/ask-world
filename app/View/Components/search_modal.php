fb732e983b19af31c9f472e12855b6f81171338e
tanvir-sadi <tanvir.sadi10@northsouth.edu>
2022-11-28 20:56:55 +0600

    Modal

---
+++ app/View/Components/search_modal.php
@@ -0,0 +1,8 @@
+<div id="searchModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-20 inset-x-0 z-100 w-full md:h-full bg-gray-200 bg-opacity-70">
+        <div class="relative w-full max-w-3xl h-full md:h-auto mx-auto">
+                <!-- Modal content -->
+                <div class="relative bg-white rounded-lg shadow ">
+                        <ul class="px-2 divide-y" id="suggestion"></ul>
+                </div>
+        </div>
+</div>
